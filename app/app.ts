2562bf2c0381653869f9fe98584463e3179f4069
wilsonpiress <wilsonpires.js@gmail.com>
2023-01-03 09:55:30 -0300

    Integrando a View à página

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,4 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
-import { NegociacoesView } from './views/negociacoes-view.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
@@ -7,7 +6,3 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
-
-const negociacoesView = new NegociacoesView
-const template = negociacoesView.template();
-console.log(template);
--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,4 +1,12 @@
 export class NegociacoesView {
+
+    private elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+
+    }
+
     template(): string {
         return `
         <table class="table table-hover table-bordered">
@@ -14,5 +22,9 @@ export class NegociacoesView {
         </table>
 
         `;
+    }
+
+    update(): void {
+        this.elemento.innerHTML = this.template();
     }
 }
